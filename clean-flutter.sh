f5a0621a6da207d07107718aafb44756f4e0dee4
Philippe Dupuis <philippe@braver.health>
2024-03-12 13:59:18 -0400

    Cleanup

---
+++ clean-flutter.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+rm -rf android
+rm -rf third_party
+rm -rf tool
+rm -rf windows
+rm -rf darwin
+rm -rf linux
+rm -rf example
+rm -rf lib/src/flutter
+rm -rf lib/src/third_party
+rm lib/src/testing/utils/err_stack_ffi.dart
+rm -rf lib/src/impl_ffi
+rm -rf lib/src/boringssl
